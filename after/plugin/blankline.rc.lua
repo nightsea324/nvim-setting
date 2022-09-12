b612d20eff6a51caa22cc435edf2ed47842346af
NightSea <sweetmilkshake0324@gmail.com>
2022-09-12 09:10:51 +0800

    update

--- after/plugin/blankline.rc.lua
+++ after/plugin/blankline.rc.lua
@@ -1,22 +1,23 @@
 local status, blankline = pcall(require, "indent_blankline")
 if (not status) then return end
 
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 guibg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B guibg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 guibg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 guibg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF guibg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD guibg=#C678DD gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineContextChar1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineContextChar2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineContextChar3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineContextChar4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineContextChar5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineContextChar6 guifg=#C678DD gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar1 guibg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar2 guibg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar3 guibg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar4 guibg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar5 guibg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar6 guibg=#C678DD gui=nocombine]]
 
+vim.g.indent_blankline_show_current_context = true
 
 blankline.setup {
-  char = " ",
+  context_char = " ",
   char_highlight_list = {
     "indentblanklineindent1",
     "indentblanklineindent2",
