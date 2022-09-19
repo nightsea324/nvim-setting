0e857e2a43cb8c48b04af40dc3ceb4d160b57dfe
NightSea <sweetmilkshake0324@gmail.com>
2022-09-19 16:53:52 +0800

    update

--- after/plugin/nvimtree.rc.lua
+++ after/plugin/nvimtree.rc.lua
@@ -54,8 +54,8 @@ nvim_tree.setup({
     show_on_dirs = true,
     debounce_delay = 50,
     icons = {
-      hint = "",
-      info = "",
+      hint = "",
+      info = "",
       warning = "",
       error = "",
     },
